5c07fb0a7264139ff8ed6d2ec7dbdaf0d96a82c5
Alfonso Presa <alfonso.presa@bbva.com>
2020-09-25 14:49:58 +0200

    fix(): control concurrent invocations

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const {promisify} = require("util");
 const {join} = require("path");
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
+const SingleInstance = require('single-instance');
+const file = join(homedir, '.aws_creds_cache');
 
 async function getOutput(command) {
     return new Promise((resolve, reject) => {
         let result = '';
-        const child = spawn(command[0], command.slice(1));
+        const child = spawn(command[0], command.slice(1), {env: {
+            ...process.env,
+            AWS_PROFILE: undefined
+        }});
         child.stdout.on('data', function(data) {
             result+=data;
         });
@@ -22,9 +27,11 @@ async function getOutput(command) {
     });
 }
 
-async function awsume(profile) {
+async function updateCreds(creds, profile) {
     const command = await getOutput(['aws','configure','get','credential_process','--profile='+profile]);
-    return getOutput(['sh','-c',command]);
+    const response = await getOutput(command.trim().split(" "));
+    creds[profile] = JSON.parse(response);
+    await writeFile(file, JSON.stringify(creds));
 }
 
 function expired(credential) {
@@ -34,26 +41,29 @@ function expired(credential) {
 }
 
 async function doIt(profile) {
-    let save = false;
-    let creds = {};
-    const file = join(homedir, '.aws_creds_cache');
+    const locker = new SingleInstance('aws-credentials-cache-' + profile);
+    let flag = true;
+    while(flag) {
+        try {
+            await locker.lock();
+            flag = false;
+        } catch(e) {
+            await new Promise(r => setTimeout(r, 100));
+        }
+    }
 
+    let creds = {};
     try {
         creds = JSON.parse(await readFile(file));
         if(!creds || !creds[profile] || expired(creds[profile])) {
-            save=true;
-            creds[profile] = JSON.parse(await awsume(profile));
+            await updateCreds(creds, profile);
         }
-
     } catch(e) {
-        save=true;
-        creds[profile] = JSON.parse(await awsume(profile));
+        await updateCreds(creds, profile);
     }
 
-    if(save) {
-        writeFile(file, JSON.stringify(creds));
-    }
     console.log(JSON.stringify(creds[profile], null, 2));
+    await locker.unlock();
 }
 
 doIt(process.argv[2]);
